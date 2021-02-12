2e78b1b66836b6a15389e927c07326103ebcdfa8
Aaron Kling <webgeek1234@gmail.com>
2021-02-11 22:03:22 -0600

    nx: switch to btlinux
    bluedroid has issue on nx devices!
    
    Change-Id: I9bb0f0b39184e521285949316b5912f9ef578fe4

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -69,6 +69,14 @@ LOCAL_MODULE_CLASS  := EXECUTABLES
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE               := nx_bt.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := nx_bt.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE        := jc_setup
 LOCAL_SRC_FILES     := jc_setup.sh
 LOCAL_MODULE_SUFFIX := .sh
