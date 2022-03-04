bc56ad0e79e854c9b61db54aee7bebc80105d75c
Thomas Makin <halorocker89@gmail.com>
2022-03-04 05:21:46 +0000

    dockservice: update log statements
    - Adds log statement on entering HDMI disconnected block
    - Changes display index logging from error to information

--- devicesettings/src/org/lineageos/settings/device/DockService.java
+++ devicesettings/src/org/lineageos/settings/device/DockService.java
@@ -161,7 +161,7 @@ public class DockService extends Service {
                                 final Point displaySize = new Point();
 
                                 for (int i = 0; i < displays.length; i++)
-                                    Log.e(TAG, "Display idx: " + String.valueOf(i) + " id: " + String.valueOf(displays[i].getDisplayId()));
+                                    Log.i(TAG, "Display idx: " + String.valueOf(i) + " id: " + String.valueOf(displays[i].getDisplayId()));
 
                                 // Preserve user/default set built-in display resolution.
                                 mWindowManager.getBaseDisplaySize(0, displaySize);
@@ -179,6 +179,7 @@ public class DockService extends Service {
                                 mWindowManager.setForcedDisplayDensityForUser(externalDisplayId, 160, UserHandle.USER_CURRENT);
                             }
                         } else {
+                            Log.i(TAG, "HDMI disconnected");
                             if (mExternalDisplayConnected) {
                                 if (isTv) {
                                     // Restore default resolution and density for built-in display
